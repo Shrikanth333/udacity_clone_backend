bd7a8995a0a574ee0b65941db59a77478de1f7dc
shrikanth333 <shrikanthn333@gmail.com>
2020-05-04 13:23:10 +0530

    added program APIs

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app=express()
 require ("dotenv").config()
 
 
-app.get("/",(req,res)=>{
-res.send("hello")
-})
+
+app.use(express.json());
 
 
+app.use("/program/",require('./controllers/router/program.js')
+)
+app.use("/program/lesson",require('./controllers/router/programLessons.js')
+)
+app.use("/program/instructor",require('./controllers/router/programInstructor.js')
+)
 const port=process.env.PORT || 8000
-
+app.use(function(err, req, res, next) {
+    
+    res.status(500).send(err.message);
+  });
 
 app.listen(port)
+
+

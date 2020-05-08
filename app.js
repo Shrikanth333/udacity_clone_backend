5f382e8cb5742e3dd1bac74226f6e1aab82d2754
keerthimalan <keerthimalan@gmail.com>
2020-05-08 12:16:22 +0530

    Enable CORS and add it as dependency

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5050;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/admins', require('./routes/adminRoutes'));
-
-
 app.use('/programs', require('./routes/program.js'));
 
 app.use(function (err, req, res, next) {
-  res.status(500).send('Bad Request');
+	res.status(500).send('Bad Request');
 });
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-

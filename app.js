94c056e0ed23a2112d23110b3811856dde30e083
shrikanth333 <shrikanthn333@gmail.com>
2020-05-09 13:51:05 +0530

    added jwt authentication feature

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const {jwtVerify}=require("./authorization/jwtVerify.js")
 const port = process.env.PORT || 5050;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors())
+
 app.use('/api/users', require('./routes/userRoutes'));
+app.use(jwtVerify)
 app.use('/api/admins', require('./routes/adminRoutes'));
 app.use('/programs', require('./routes/program.js'));
 

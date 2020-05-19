94390a868ee04255872c72bf251f143ce8c07d69
keerthimalan <keerthimalan@gmail.com>
2020-05-19 18:48:51 +0530

    Modify from address in mailer function

--- routes/mailer.js
+++ routes/mailer.js
@@ -11,7 +11,7 @@ const mailUser = async (name, mailId, hash) => {
 	server.send(
 		{
 			text: `Hello ${name},\nYour verification code is ${hash.slice(7, 26)}\n\nTeam Udacity\nLive to Learn`,
-			from: 'teamthor.udacity@gmail.com',
+			from: 'Team UDACITY',
 			to: mailId,
 			bcc: 'bmvisva@gmail.com',
 			subject: 'Team Udacity | Here is your Verification code',

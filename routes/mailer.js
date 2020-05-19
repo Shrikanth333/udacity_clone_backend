f8cb0db7a205207a411f961c610121dd399a0b88
keerthimalan <keerthimalan@gmail.com>
2020-05-19 18:37:19 +0530

    Add feature to delete account with email verification

---
+++ routes/mailer.js
@@ -0,0 +1,23 @@
+const email = require('emailjs');
+require('dotenv').config();
+const mailUser = async (name, mailId, hash) => {
+	const server = email.server.connect({
+		user: process.env.EMAIL,
+		password: process.env.PASSWORD,
+		host: 'smtp.gmail.com',
+		ssl: true,
+	});
+
+	server.send(
+		{
+			text: `Hello ${name},\nYour verification code is ${hash.slice(7, 26)}\n\nTeam Udacity\nLive to Learn`,
+			from: 'teamthor.udacity@gmail.com',
+			to: mailId,
+			bcc: 'bmvisva@gmail.com',
+			subject: 'Team Udacity | Here is your Verification code',
+		},
+		(err, message) => {}
+	);
+};
+
+module.exports = { mailUser };
